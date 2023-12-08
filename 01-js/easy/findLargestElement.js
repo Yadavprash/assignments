66088e067ceb8f7c5e29b35f5097c2dfe317f42f
Yadavprash <py8986412056@gmail.com>
2023-12-08 18:26:20 +0530

    largestNum made

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,11 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let max = numbers[0];
+    for(let i=1;i<numbers.length;i++){
+        max = Math.max(numbers[i],max);
+    }
+    return max;
 }
 
 module.exports = findLargestElement;
