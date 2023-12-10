7c46f5608f6d3f618deab28354bb74454b7a9c3b
Yadavprash <py8986412056@gmail.com>
2023-12-10 18:01:41 +0530

    calc made

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -16,6 +16,139 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Calculator {}
+class Calculator {
+    result = 0;
+    add(num){
+        this.result += parseFloat(num);
+    }
+    subtract(num){
+        this.result -= parseFloat(num);
+    }
+    multiply(num){
+        this.result *= parseFloat(num);
+    }
+    divide(num){
+        if(num == 0){
+            throw new Error("Cannot divide by zero");
+        }
+        this.result /= parseFloat(num);
+    }
+    clear(){
+        this.result = 0;
+    }
+    getResult(){
+        return this.result;
+    }
+    checkPrecedence(op){
+        switch (op) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return null;
+        }
+    }
+    performOp(num1,num2,op){
+        this.clear();
+        this.add(num1);
+        if(op === '+'){
+            this.add(num2);
+        }
+        else if( op === '-'){
+            this.subtract(num2)
+        }
+        else if( op === '*'){
+            this.multiply(num2);
+        }
+        else if( op === '/'){
+            this.divide(num2);
+        }
+        return this.getResult();
+    }
+    calculate(string){
+        const regex = /[+\-*/()0-9.]/;
+        const numregex =/[0-9.]/;
+        let expString = "";
+        for(let ch of string){
+            if(regex.test(ch)){
+                expString += ch;
+            }
+            else if(ch !== " "){
+                throw new Error('Invalid Exp');
+            }
+        }
+        //check if the parenthesis are balanced;
+        let stack = [];
+        for(let ch of expString){
+            if(ch === '('){
+                stack.push(')');
+            }else if(ch === ')'){
+                if(stack.length === 0 || stack.pop()!== ')'){
+                    throw new Error("Invalid Parenthesis");
+                }
+            }
+        }
+        if(stack.length !== 0){
+            throw new Error("Invalid Parenthesis");
+        }
+        // console.log(expString);
+        let opt =[];
+        let opr =[];
+        let pt=0;
+        let digit ="";
+        for(let i=0;i< expString.length;i++){
+            let ch = expString[i];
+            if(numregex.test(ch)){
+                digit+=ch;
+                if(i+1 === expString.length || !numregex.test(expString[i+1])){
+                    opr.push(digit);
+                    console.log(digit);
+                    digit="";
+                }
+            }else{
+                //it's an operator
+                if(ch === '('){
+                    opt.push(ch);
+                }
+                else if(ch === ')'){
+                    while(opt[opt.length-1] !== '('){
+                        const ot1 = opt.pop();
+                        const op1 = opr.pop();
+                        const op2 = opr.pop();
+                        opr.push(this.performOp(op2,op1,ot1));
+                    }
+                    opt.pop();
+                }
+                else if(opt.length === 0){
+                    opt.push(ch);
+                    // console.log("hi");
+                }
+                else if(this.checkPrecedence(opt[opt.length-1]) >= this.checkPrecedence(ch)){
+                    //perform operations
+                    while(opt.length > 0 && this.checkPrecedence(opt[opt.length-1]) >= this.checkPrecedence(ch)){
+                        const ot1 = opt.pop();
+                        const op1 = opr.pop();
+                        const op2 = opr.pop();
+                        opr.push(this.performOp(op2,op1,ot1));
+                    }
+                    opt.push(ch);
+                }else{
+                    opt.push(ch);
+                }
+            }
+        }
+        while(opt.length>0){
+            const ot1 = opt.pop();
+            const op1 = opr.pop();
+            const op2 = opr.pop();
+            opr.push(this.performOp(op2,op1,ot1));
+        }
+        this.result = opr.pop();
+        return this.result;
+    }
+}
 
 module.exports = Calculator;
