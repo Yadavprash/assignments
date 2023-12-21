d85cd0d647b6ec8145d56fe40d9689f9f4e1998f
Yadavprash <py8986412056@gmail.com>
2023-12-21 10:59:59 +0530

    promisified set timeout

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -1,8 +1,16 @@
 /*
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
-
 function wait(n) {
+    return new Promise((resolve, reject) => {
+        const res = `Resolved after ${n / 1000} Seconds.`
+        setTimeout(() => {
+            resolve(res);
+        }, n);
+    });
 }
-
+// wait(3000)
+//     .then((data)=>{
+//         console.log(data);
+//     });
 module.exports = wait;
