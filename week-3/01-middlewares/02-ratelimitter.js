03dbf2dbdb1fd4c14e171335d84dfb3fd1901d41
Yadavprash <py8986412056@gmail.com>
2023-12-27 11:05:24 +0530

    middleware asgn done

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,7 +16,20 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+function requestLimiter(req,res,next){
+    const id = req.headers['user-id'];
+    if(numberOfRequestsForUser[id] > 5){
+        res.status(404).send({error : "Request limit Exceeded!"})
+    }else{
+        if(numberOfRequestsForUser.hasOwnProperty(id))
+            numberOfRequestsForUser[id]++;
+        else numberOfRequestsForUser[id] = 1;
+        next();
+    }
+}
+app.use(requestLimiter);
 app.get('/user', function(req, res) {
+    console.log(numberOfRequestsForUser);
   res.status(200).json({ name: 'john' });
 });
 
@@ -24,4 +37,5 @@ app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
+// app.listen(3000);
 module.exports = app;
